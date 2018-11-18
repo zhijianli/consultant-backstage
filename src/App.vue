e893b592439ec49cffd99540ca18cf578db42609
“e <lizj@317hu.com>
2018-11-18 19:00:52 +0800

    提交“藏龙”项目后台代码

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,85 @@
 <template>
+<div id="app">
+    <router-view></router-view>
+</div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss">
+body {
+    margin: 0;
+    padding: 0;
+    font-size: 14px;
+    -webkit-font-smoothing: antialiased;
+    overflow: scroll;
+    overflow-y: hidden;
+}
+
+#app {
+    position: absolute;
+    top: 0;
+    bottom: 0;
+    width: 100%;
+}
+
+.el-submenu [class^=fa] {
+    vertical-align: baseline;
+    margin-right: 10px;
+}
+
+.el-menu-item [class^=fa] {
+    vertical-align: baseline;
+    margin-right: 10px;
+}
+
+.toolbar {
+    background: #f2f2f2;
+    padding: 10px;
+    margin: 10px 0;
+    .el-form-item {
+        margin-bottom: 10px;
+    }
+}
+
+</style>
+
+
+<!-- <template>
   <div id="app">
     <img src="./assets/logo.png">
-    <router-view/>
+    <h1>{{ msg }}</h1>
+    <h2>Essential Links</h2>
+    <ul>
+      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
+      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
+      <li><a href="https://chat.vuejs.org" target="_blank">Community Chat</a></li>
+      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
+    </ul>
+    <h2>Ecosystem</h2>
+    <ul>
+      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
+      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
+      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
+      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
+    </ul>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'App'
+  name: 'app',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
 }
 </script>
 
-<style>
+<style lang="scss">
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -20,4 +88,22 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
-</style>
+
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style> -->
--- src/main.js
+++ src/main.js
@@ -1,15 +1,40 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import Vue from 'vue'
-import App from './App'
-import router from './router'
+import Vue from 'vue';
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+import Router from 'vue-router';
+import App from './App.vue';
+import 'font-awesome/css/font-awesome.min.css'
 
-Vue.config.productionTip = false
 
-/* eslint-disable no-new */
+Vue.use(ElementUI);
+Vue.use(Router);
+
+router.beforeEach((to, from, next) => {
+    if (to.path == '/login') {
+        sessionStorage.removeItem('user');
+    }
+    let user = JSON.parse(sessionStorage.getItem('user'));
+    if (!user && to.path != '/login') {
+        next({
+            path: '/login'
+        });
+    } else {
+        next();
+    }
+});
+
+// 引入路由
+import router from "./router.js"
 new Vue({
-  el: '#app',
-  router,
-  components: { App },
-  template: '<App/>'
-})
+    el: '#app',
+    router, // 注入到根实例中
+    render: h => h(App)
+});
+
+// import Vue from 'vue'
+// import App from './App.vue'
+
+// new Vue({
+//   el: '#app',
+//   render: h => h(App)
+// })
